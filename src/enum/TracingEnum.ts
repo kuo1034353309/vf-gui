faf2eac81f16af0c53e09eb0ad3490494865afb7
yangjiehui <yangjiehui@vipkid.com.cn>
2020-07-15 19:43:03 +0800

    笔画临摹修改:1.新增严格模式,使用于精准临摹(slider中的tracing需求);2.优化本地绘图性能

--- src/enum/TracingEnum.ts
+++ src/enum/TracingEnum.ts
@@ -1,12 +1,14 @@
 export const enum Operate{
     Add, //添加
-    Clear  //清除
+    Clear,  //清除
+    Remove  //移除
 }
 
 export const enum Mode{
     Check,   //检查模式，判断轨迹是否正确
     Teach,   //教学模式，一笔一划教学
-    Auto     //自动播放
+    Auto,     //自动播放
+    Strict   //严格模式，需要严格按每个笔画的顺序书写，否则不会被书写。该笔画书写错误时会被清除
 }
 
 export const enum Result{
